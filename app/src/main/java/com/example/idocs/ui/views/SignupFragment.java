e24d59eb22056116f98fc4cdc9027c0adca2ae22
zain2323 <zainsiddiqui2323@gmail.com>
2022-04-25 23:40:44 +0500

    Added login and signup fragments and changed the app logo.

---
+++ app/src/main/java/com/example/idocs/ui/views/SignupFragment.java
@@ -0,0 +1,27 @@
+package com.example.idocs.ui.views;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.idocs.R;
+
+public class SignupFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_signup, container, false);
+    }
+}
